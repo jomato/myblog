63ed49ff3a2a4d796583863db585ee7aff27ab73
jh <jhmailnet@gmail.com>
2017-05-04 14:54:58 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField('标题', max_length=70)
+    body = models.TextField('正文')
+    created_time = models.DateField('创建时间')
+    modified_time = models.DateTimeField('修改时间')
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category, verbose_name='分类')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='标签')
+    author = models.ForeignKey(User, verbose_name='作者')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.id})
